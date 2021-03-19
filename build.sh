056c0091207624790fdb1b1bbffde6bcce54f50e
humbinal <huangbin1020@126.com>
2021-03-19 22:23:55 +0800

    test build

--- build.sh
+++ build.sh
@@ -9,14 +9,16 @@ fi
 GITHUB_WORKSPACE="$1"
 
 echo "INFO: start build..."
-echo "INFO: work space dir: $GITHUB_WORKSPACE"
+echo "INFO: CODE_SERVER_VERSION: ${CODE_SERVER_VERSION}"
+echo "INFO: VERSION: ${VERSION}"
+echo "INFO: work space dir: ${GITHUB_WORKSPACE}"
 mkdir -p /usr/local/code-server
 mkdir -p /usr/local/code-server-extensions
 mkdir -p /usr/local/code-server-users
 
 mkdir -p /tmp
-wget -P /tmp https://github.com/cdr/code-server/releases/download/v3.9.1/code-server-3.9.1-linux-amd64.tar.gz
-tar zxf /tmp/code-server-3.9.1-linux-amd64.tar.gz --strip-components 1 -C /usr/local/code-server
+wget -P /tmp "https://github.com/cdr/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server-${CODE_SERVER_VERSION}-linux-amd64.tar.gz"
+tar zxf /tmp/code-server-${CODE_SERVER_VERSION}-linux-amd64.tar.gz --strip-components 1 -C /usr/local/code-server
 
 /usr/local/code-server/bin/code-server \
   --extensions-dir /usr/local/code-server-extensions \
@@ -45,20 +47,23 @@ echo "INFO: ls -lh /usr/local/code-server-users/User"
 ls -lh /usr/local/code-server-users/User
 
 
-#output
-FINAL_NAME=code-server-extensions-3.9.1
-TARGET_DIR=$GITHUB_WORKSPACE/target
-OUTPUT_DIR=${TARGET_DIR}/${FINAL_NAME}
-mkdir -p ${OUTPUT_DIR}
+if [[ -z ${VERSION} ]]; then
+  echo "INFO: build for ci finished."
+else
+  #output
+  FINAL_NAME=code-server-v${CODE_SERVER_VERSION}-extensions-${VERSION}
+  TARGET_DIR=$GITHUB_WORKSPACE/target
+  OUTPUT_DIR=${TARGET_DIR}/${FINAL_NAME}
+  mkdir -p ${OUTPUT_DIR}
 
-#copy assest
-/bin/cp -rf /usr/local/code-server-users/languagepacks.json ${OUTPUT_DIR}/
-/bin/cp -rf /usr/local/code-server-extensions ${OUTPUT_DIR}/
+  #copy assest
+  /bin/cp -rf /usr/local/code-server-users/languagepacks.json ${OUTPUT_DIR}/
+  /bin/cp -rf /usr/local/code-server-extensions ${OUTPUT_DIR}/
 
-#build tarball
-tar zcpf ${TARGET_DIR}/${FINAL_NAME}.tar.gz -C ${TARGET_DIR} ${FINAL_NAME}
-chmod 777 ${TARGET_DIR}/${FINAL_NAME}.tar.gz
-echo "INFO: ls -lh ${TARGET_DIR}"
-ls -lh ${TARGET_DIR}
-
-echo "INFO: build finished."
+  #build tarball
+  tar zcpf ${TARGET_DIR}/${FINAL_NAME}.tar.gz -C ${TARGET_DIR} ${FINAL_NAME}
+  chmod 777 ${TARGET_DIR}/${FINAL_NAME}.tar.gz
+  echo "INFO: ls -lh ${TARGET_DIR}"
+  ls -lh ${TARGET_DIR}
+  echo "INFO: build for release finished."
+fi
